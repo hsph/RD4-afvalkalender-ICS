83efe4fcf0e2b2fb87df30ba1ff3847394460065
Sander Lienaerts <sander@lienaerts.nl>
2019-06-23 13:12:24 +0200

    Initial commit

---
+++ ical.js
@@ -0,0 +1,43 @@
+const ical = require('ical-generator');
+const cal = ical().timezone('Europe/Amsterdam');
+  
+var months = {
+    'januari' : '01',
+    'februari' : '02',
+    'maart' : '03',
+    'april' : '04',
+    'mei' : '05',
+    'juni' : '06',
+    'juli' : '07',
+    'augustus' : '08',
+    'september' : '09',
+    'oktober' : '10',
+    'november' : '11',
+    'december' : '12'
+}
+
+'use strict';
+
+const fs = require('fs');
+const moment = require('moment');
+
+let rawdata = fs.readFileSync('calendar-items.json');  
+let items = JSON.parse(rawdata);  
+
+function parseDate(input) {
+    var parts = input.split(' ');
+    var year = parts[3];
+    var month = months[parts[2]];
+    var day = parts[1];
+    return moment(year + '-' + month + '-' + day, 'YYYY-MM-DD')
+  }
+
+items.forEach(function(key, index){
+    var d = parseDate(key.FIELD1);
+    const event = cal.createEvent();
+    event.start(d);
+    event.allDay(true);
+    event.summary(key.FIELD2);
+});
+
+cal.saveSync('calendar-items.ics');
